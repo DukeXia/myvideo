d9b137503676dca177e8fafa9d30cd2150b270e6
lixinyi <244lixinyi@163.com>
2016-06-07 18:29:56 +0800

    ij

--- myvideo-web/src/main/webapp/static/js/index.js
+++ myvideo-web/src/main/webapp/static/js/index.js
@@ -1,26 +1,63 @@
 /**
  * Created by Tane on 2016/6/3.
  */
-$(document).ready(function(){
+
+$(function(){
     //填充分类视频数据
     var pageNo=1; //页数
     var pageSize=6 //每页条数
-    //填充分类信息
+    //填充分类详细信息
+    $.ajax({
+        url:'/getCategoryList',
+        success:function(data){
+            var categoryList=data.data.categoryBeanList;
+            var len=categoryList.length;
+            var navStr='';
+            for(var i=0;i<len;i++){
+                navStr += '<li data-type="'+categoryList[i].id+'">'+categoryList[i].name+'</li>';
+            }
+            $('#navbar').append(navStr);
+        }
+    });
     $.ajax({
         url:'/getVideoList',
         dataType:'json',
-        data:{'categoryId':1,'pageNo':pageNo,'pageSize':pageSize},
+        data:{'categoryNum':5,'videoNum':4},
         success:function(data){
             console.log(data);
             if(data.result=='100'){
+                var str='';
+                var cateData=data.data.preVideoList;
+                var len= cateData.length;
+                for(var i=0;i<len;i++){
+                    str+='<div class="items"><div class="item-con"><div class="item-title">' +
+                    cateData[i][0].category +'</div><div class="video-list">';
+                    var innerLen=cateData[i].length;
+                    for(var j=0;j<innerLen;j++){
+                        str+=
+                        '<div class="video-single">'+
+                        '<img src="'+cateData[i][j].pic+'" width="225" height="110" alt="视频封面" title="视频封面">'+
+                        '<div class="video-layout">' +
+                        '<p>'+cateData[i][j].title+'</p>' +
+                        '<p>'+cateData[i][j].desc+'</p>' +
+                        '<p>'+cateData[i][j].created+'</p>' +
+                        '<span class="player-btn" data-link="'+cateData[i][j].url+'">播放</span>' +
 
+                        '</div>'+
+
+                        '</div>';
+
+                    }
+                    str += "</div></div></div>";
+                }
+                $('.main-box').append(str);
             }
         },
         error:function(data){
 
         }
     });
-
+    //login
     $('#login-trigger').click(function(){
         $(this).next('#login-content').slideToggle();
         $(this).toggleClass('active');
@@ -40,17 +77,25 @@ $(document).ready(function(){
                     //window.open('http://127.0.0.1:8095/index.html');
                     $('#login-trigger').html('admin');
                     $('#login-content').slideUp();
+                }else{
+                    alert('登录失败：'+data.message);
                 }
 
             }
         })
     });
-    $(".type-list").find("li").hover(function(){
-        $('.dot').css('width','60px');
-        var  listWidth = $(".type-list").width();
+    //navbar
+    $(document).on("click","#navbar li",function(e){
+        e.stopPropagation();
+        var  listWidth = $("#navbar").width();
         var  remain_left = ($(window).width()-listWidth)/2;
         var _left = $(this).offset().left-remain_left;
         $(".type-list").find(".dot").animate({left:_left},10);
+    });
+    //backTop
+    $(".foot .back").click(function () {
+        $("body,html").animate({scrollTop: 0}, 500)
+        return false;
     })
 
 });
--- myvideo-web/target/myblog-web/static/js/index.js
+++ myvideo-web/target/myblog-web/static/js/index.js
@@ -1,8 +1,25 @@
 /**
  * Created by Tane on 2016/6/3.
  */
-//��¼
 $(document).ready(function(){
+    //填充分类视频数据
+    var pageNo=1; //页数
+    var pageSize=6 //每页条数
+    //填充分类信息
+    $.ajax({
+        url:'/getVideoList',
+        dataType:'json',
+        data:{'categoryId':1,'pageNo':pageNo,'pageSize':pageSize},
+        success:function(data){
+            console.log(data);
+            if(data.result=='100'){
+
+            }
+        },
+        error:function(data){
+
+        }
+    });
 
     $('#login-trigger').click(function(){
         $(this).next('#login-content').slideToggle();
@@ -10,9 +27,6 @@ $(document).ready(function(){
 
         if ($(this).hasClass('active')) $(this).find('span').html('&#x25B2;')
         else $(this).find('span').html('&#x25BC;')
-<<<<<<< HEAD
-    })
-=======
     });
     $('#submit').click(function(){
         var username=$('#username').val();
@@ -22,7 +36,11 @@ $(document).ready(function(){
             data:{'username':username,'password':password},
             dataType:'json',
             success: function (data) {
-                console.log(data)
+                if(data.result=='100'){
+                    //window.open('http://127.0.0.1:8095/index.html');
+                    $('#login-trigger').html('admin');
+                    $('#login-content').slideUp();
+                }
 
             }
         })
@@ -35,5 +53,4 @@ $(document).ready(function(){
         $(".type-list").find(".dot").animate({left:_left},10);
     })
 
->>>>>>> aed92bf18a5425d91d772a65a804dc821f8f08e3
 });
