d665472a2d43e724b0ddb46f5527686b53154382
lixinyi <244lixinyi@163.com>
2016-06-10 18:56:48 +0800

    is ok

--- myvideo-web/src/main/webapp/static/js/index.js
+++ myvideo-web/src/main/webapp/static/js/index.js
@@ -24,12 +24,14 @@ $(function(){
         dataType:'json',
         data:{'categoryNum':5,'videoNum':4},
         success:function(data){
-            console.log(data);
             if(data.result=='100'){
                 var str='';
                 var cateData=data.data.preVideoList;
                 var len= cateData.length;
                 for(var i=0;i<len;i++){
+                    if(cateData[i].length ==0){
+                        continue;
+                    }
                     str+='<div class="items"><div class="item-con"><div class="item-title">' +
                     cateData[i][0].category +'</div><div class="video-list">';
                     var innerLen=cateData[i].length;
@@ -41,7 +43,7 @@ $(function(){
                         '<p>'+cateData[i][j].title+'</p>' +
                         '<p>'+cateData[i][j].desc+'</p>' +
                         '<p>'+cateData[i][j].created+'</p>' +
-                        '<span class="player-btn" data-link="'+cateData[i][j].url+'">播放</span>' +
+                        '<span class="player-btn" data-id="'+cateData[i][j].id+'">播放</span>' +
 
                         '</div>'+
 
@@ -57,45 +59,7 @@ $(function(){
 
         }
     });
-    //login
-    $('#login-trigger').click(function(){
-        $(this).next('#login-content').slideToggle();
-        $(this).toggleClass('active');
 
-        if ($(this).hasClass('active')) $(this).find('span').html('&#x25B2;')
-        else $(this).find('span').html('&#x25BC;')
-    });
-    $('#submit').click(function(){
-        var username=$('#username').val();
-        var password=$('#password').val();
-        $.ajax({
-            url:'/user/login',
-            data:{'username':username,'password':password},
-            dataType:'json',
-            success: function (data) {
-                if(data.result=='100'){
-                    //window.open('http://127.0.0.1:8095/index.html');
-                    $('#login-trigger').html('admin');
-                    $('#login-content').slideUp();
-                }else{
-                    alert('登录失败：'+data.message);
-                }
 
-            }
-        })
-    });
-    //navbar
-    $(document).on("click","#navbar li",function(e){
-        e.stopPropagation();
-        var  listWidth = $("#navbar").width();
-        var  remain_left = ($(window).width()-listWidth)/2;
-        var _left = $(this).offset().left-remain_left;
-        $(".type-list").find(".dot").animate({left:_left},10);
-    });
-    //backTop
-    $(".foot .back").click(function () {
-        $("body,html").animate({scrollTop: 0}, 500)
-        return false;
-    })
 
 });
